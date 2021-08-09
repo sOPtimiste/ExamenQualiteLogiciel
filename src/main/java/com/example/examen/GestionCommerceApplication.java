b0ae7d0a2b949b78e83b633c2f8479dd31b174ce
sOptimiste <mbackesall33@gmail.com>
2021-08-09 13:49:56 +0200

    first commit

---
+++ src/main/java/com/example/examen/GestionCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCommerceApplication.class, args);
+	}
+
+}
